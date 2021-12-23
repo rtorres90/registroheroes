82f479d4680e68a39ca4c6f04b03cdd27910c2d5
Roberto Torres <roberto.torres.dev@gmail.com>
2021-12-23 16:38:06 -0300

    adding listener and intent

--- app/src/main/java/com/example/registrosuperheroes/MainActivity.kt
+++ app/src/main/java/com/example/registrosuperheroes/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.registrosuperheroes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.registrosuperheroes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.saveButton.setOnClickListener {
+            openDetailActivity()
+        }
+    }
+
+    private fun openDetailActivity(){
+        val intent = Intent(this, DetailActivity::class.java)
+        startActivity(intent)
     }
 }
