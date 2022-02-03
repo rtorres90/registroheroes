1599e73d526778677ab9be8c7a00d840a79a9f79
Roberto Torres <roberto.torres.dev@gmail.com>
2022-02-03 15:22:21 -0300

    using Activity Result API

--- app/src/main/java/com/example/registrosuperheroes/DetailActivity.kt
+++ app/src/main/java/com/example/registrosuperheroes/DetailActivity.kt
@@ -1,13 +1,17 @@
 package com.example.registrosuperheroes
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.registrosuperheroes.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
-    companion object{
+    companion object {
         const val SUPERHERO_KEY = "superhero"
+        const val BITMAP_KEY = "bitmap"
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityDetailBinding.inflate(layoutInflater)
@@ -15,10 +19,11 @@ class DetailActivity : AppCompatActivity() {
 
         val bundle = intent.extras!!
         val superHero = bundle.getParcelable<Superhero>(SUPERHERO_KEY)!!
+        val bitmapDirectory = bundle.getString(BITMAP_KEY)!!
 
-        binding.heroName.text = superHero.name
-        binding.alterEgoText.text = superHero.alterEgo
-        binding.bioText.text = superHero.bio
-        binding.detailPoderBar.rating = superHero.power
+        val bitmap = BitmapFactory.decodeFile(bitmapDirectory)
+
+        binding.imageView.setImageBitmap(bitmap)
+        binding.superhero = superHero
     }
 }
--- app/src/main/java/com/example/registrosuperheroes/MainActivity.kt
+++ app/src/main/java/com/example/registrosuperheroes/MainActivity.kt
@@ -1,28 +1,78 @@
 package com.example.registrosuperheroes
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import com.example.registrosuperheroes.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var heroImage: ImageView
+    private var heroBitmap: Bitmap? = null
+    private var picturePath = ""
+
+    val getContent =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+            if (success && picturePath.isNotEmpty()) {
+                heroBitmap = BitmapFactory.decodeFile(picturePath)
+                heroImage.setImageBitmap(heroBitmap!!)
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.saveButton.setOnClickListener {
-            var superHeroName = binding.heroNameEdit.text.toString()
-            var alterEgo = binding.alterEgoEdit.text.toString()
-            var bio = binding.bioEdit.text.toString()
-            var power = binding.poderBar.rating
+            val superHeroName = binding.heroNameEdit.text.toString()
+            val alterEgo = binding.alterEgoEdit.text.toString()
+            val bio = binding.bioEdit.text.toString()
+            val power = binding.poderBar.rating
             val hero = Superhero(superHeroName, alterEgo, bio, power)
             openDetailActivity(hero)
         }
+        heroImage = binding.heroImage
+        binding.heroImage.setOnClickListener {
+            openCamera()
+        }
+    }
+
+    private fun openCamera() {
+        val file = createImageFile()
+        val uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            FileProvider.getUriForFile(
+                this,
+                "$packageName.provider",
+                file
+            )
+        } else {
+            Uri.fromFile(file)
+        }
+        getContent.launch(uri)
+    }
+
+    private fun createImageFile(): File {
+        val fileName = "superhero_image"
+        val fileDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val file = File.createTempFile(fileName, ".jpg", fileDirectory)
+        picturePath = file.absolutePath
+        return file
     }
 
     private fun openDetailActivity(hero: Superhero) {
         val intent = Intent(this, DetailActivity::class.java)
         intent.putExtra(DetailActivity.SUPERHERO_KEY, hero)
+        intent.putExtra(DetailActivity.BITMAP_KEY, picturePath)
         startActivity(intent)
     }
+
 }
